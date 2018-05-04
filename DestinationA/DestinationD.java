a0986664a3c1d5149a1baf5241dabe6f0842510b
Chytra K R <34940260+chytrakr@users.noreply.github.com>
2018-05-04 12:09:12 +0530

    Update DestinationD.java
    Syntax changes

--- DestinationA/DestinationD.java
+++ DestinationA/DestinationD.java
@@ -29,7 +29,6 @@ import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.Statement;
-
 import javax.sound.sampled.AudioFormat.Encoding;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -44,155 +43,107 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class DestinationD implements ActionListener {
-	public Font f = new Font("Times new roman", Font.BOLD, 20);
+    public Font f = new Font("Times new roman", Font.BOLD, 20);
 
-	public JLabel T4 = new JLabel();
-	
-	public JTextArea tf = new JTextArea();
-	public JScrollPane pane = new JScrollPane();
-	
-	JPanel jp = new JPanel();
-	JPanel jp1 = new JPanel();
+    public JLabel T4 = new JLabel();
 
-	public JFrame jf;
-	public Container c;
-	
-	DestinationD() {
-		jf = new JFrame("Destination D::Behavioral Malware Detection in Delay Tolerant Networks");
-		c = jf.getContentPane();
-		c.setLayout(null);
-		jf.setSize(500, 650);
-		 c.setBackground(new Color(139,134, 130));
+    public JTextArea tf = new JTextArea();
+    public JScrollPane pane = new JScrollPane();
 
+    JPanel jp = new JPanel();
+    JPanel jp1 = new JPanel();
 
-		jp.setBounds(40, 126, 400, 400);
-		
-		//jp1.setBackground(new Color(120,0,0));
-		//jp1.setBounds(155, 124, 723, 453);
+    public JFrame jf;
+    public Container c;
 
-		
-		
+    DestinationD() {
+        jf = new JFrame("Destination D::Behavioral Malware Detection in Delay Tolerant Networks");
+        c = jf.getContentPane();
+        c.setLayout(null);
+        jf.setSize(500, 650);
+        c.setBackground(new Color(139, 134, 130));
 
-		tf.setColumns(20);
+        jp.setBounds(40, 126, 400, 400);
 
-		tf.setForeground(Color.MAGENTA);
-	    tf.setFont(f);
-		tf.setRows(100);
-		tf.setColumns(100);
-		tf.setName("tf");
-		pane.setName("pane");
-		pane.setViewportView(tf);
-		pane.setBounds(75, 146, 350, 350);
+        //jp1.setBackground(new Color(120,0,0));
+        //jp1.setBounds(155, 124, 723, 453);
 
+        tf.setColumns(20);
+        tf.setForeground(Color.MAGENTA);
+        tf.setFont(f);
+        tf.setRows(100);
+        tf.setColumns(100);
+        tf.setName("tf");
+        pane.setName("pane");
+        pane.setViewportView(tf);
+        pane.setBounds(75, 146, 350, 350);
+        jf.show();
+        c.add(T4);
+        c.add(pane, BorderLayout.CENTER);
+        c.add(jp);
+        c.add(jp1);
+        
+        jf.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent win) {
+                System.exit(0);
+            }
+        });
+        int[] ports = new int[] {
+            4564
+        };
+        for (int i = 0; i < 1; i++) {
+            Thread th = new Thread(new PortListener(ports[i]));
+            th.start();
+        }
+    }
 
-	
-		jf.show();
+    public void actionPerformed(ActionEvent e) {
+    }
+    class PortListener implements Runnable {
 
-		
-		c.add(T4);
+        BufferedOutputStream bos = null;
 
-		c.add(pane, BorderLayout.CENTER);
-		
+        ServerSocket server;
+        Socket con;
+        BufferedReader br = null;
+        int port;
 
+        public PortListener(int port) {
+            this.port = port;
+        }
 
-		c.add(jp);
-		c.add(jp1);
+        @Override
+        public void run() {
 
-		
-		
-		
-		jf.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent win) {
-				System.exit(0);
-			}
-		});
-		
-		
-	
-		
-		int[] ports = new int[]{4564};
-		for(int i=0;i<1;i++)
-		{
-			Thread th = new Thread(new PortListener(ports[i]));
-			th.start();
-		}
-			
-		
-		
-		
+            if (this.port == 4564) {
+                try {
+                    ServerSocket server3 = new ServerSocket(4564);
+                    Socket con2;
+                    while (true) {
+                        con2 = server3.accept();
+                        DataInputStream dis11 = new DataInputStream(con2.getInputStream());
 
-	}
+                        String ct = dis11.readUTF();
+                        String fname = dis11.readUTF();
 
-	public void actionPerformed(ActionEvent e) {
-		
-			
+                        tf.setText(ct);
 
-	
-		
-		
-	
-			
-					}
-				
-			
-		
+                        PrintStream out2 = null;
+                        try {
+                            out2 = new PrintStream(new FileOutputStream("DestinationD\\" + fname));
+                            out2.print(ct);
 
-	
-
-	class PortListener implements Runnable {
-	
-		BufferedOutputStream bos = null;
-
-		ServerSocket server;
-		Socket con;
-		BufferedReader br = null;
-		int port;
-
-		public PortListener(int port) {
-			this.port = port;
-		}
-
-		@Override
-		public void run() 
-		{
-			
-			if(this.port==4564)
-			{
-				 try
-					{
-						ServerSocket server3 = new ServerSocket(4564);
-		                Socket con2;
-						while (true) 
-						{
-							con2 = server3.accept();
-							DataInputStream dis11 = new DataInputStream(con2.getInputStream());
-							
-							String ct= dis11.readUTF();
-							String fname= dis11.readUTF();
-							
-							tf.setText(ct);
-							
-							PrintStream out2 = null;
-							try {
-							    out2 = new PrintStream(new FileOutputStream("DestinationD\\"+fname));
-							    out2.print(ct);
-							    
-							}
-							finally {
-							    if (out2 != null) out2.close();
-							}
-							
-							
-							
-						}
-					}catch(Exception es){System.out.println(es);}
-			}
-			
-			
-		}
-	}
-
-	public static void main(String args[]) {
-		new DestinationD();
-	}
+                        } finally {
+                            if (out2 != null) out2.close();
+                        }
+                    }
+                } catch (Exception es) {
+                    System.out.println(es);
+                }
+            }
+        }
+    }
+    public static void main(String args[]) {
+        new DestinationD();
+    }
 }
